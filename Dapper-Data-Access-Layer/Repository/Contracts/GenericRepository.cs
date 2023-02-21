6970e3209eee63f8dce5f570e6395438e3a77187
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-02-21 19:27:24 +0200

    Start implementation the Repository Pattern

---
+++ Dapper-Data-Access-Layer/Repository/Contracts/GenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper_Data_Access_Layer.Context;
+using Dapper_Data_Access_Layer.Repository.Contracts.Interfaces;
+
+namespace Dapper_Data_Access_Layer.Repository.Contracts
+{
+    public class GenericRepository : IGenericRepository
+    {
+        private readonly DapperContext _context;
+        public GenericRepository(DapperContext context) => _context = context;
+    }
+}
