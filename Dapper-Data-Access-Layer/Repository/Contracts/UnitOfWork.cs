f1f716f16394731752749b778c32ef2c9566b9fb
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-02-21 20:39:41 +0200

    New Interfaces

---
+++ Dapper-Data-Access-Layer/Repository/Contracts/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper_Data_Access_Layer.Repository.Contracts.Interfaces;
+
+namespace Dapper_Data_Access_Layer.Repository.Contracts
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbTransaction transaction;
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            transaction?.Connection.Close();
+        }
+    }
+}
