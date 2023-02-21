19c2b3d9bc6b220995f1258c428d53765bebea39
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-02-21 19:14:05 +0200

    New Singelton and new Controllers

--- Example-API-Dapper/Program.cs
+++ Example-API-Dapper/Program.cs
@@ -1,7 +1,12 @@
+using Dapper_Data_Access_Layer.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,6 +17,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
