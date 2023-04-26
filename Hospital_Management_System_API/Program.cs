8148b7cd0561a33ea236d47a8bdeddd8a51de7c8
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-04-27 02:53:23 +0300

    New changes

---
+++ Hospital_Management_System_API/Program.cs
@@ -0,0 +1,84 @@
+using System.Data;
+using System.Reflection;
+using Bogus;
+using Dapper_Data_Access_Layer.Entities;
+using Dapper_Data_Access_Layer.Repository.Contracts;
+using Dapper_Data_Access_Layer.Repository.Contracts.Interfaces;
+using Dapper_Data_Access_Layer.Repository.RepositoryPattern;
+using Dapper_Data_Access_Layer.Repository.RepositoryPattern.Interfaces;
+using Dapper_Example_Bussines_Logic.Data_Transfer_Object.Services.Interfaces;
+using Dapper_Example_Bussines_Logic.Data_Transfer_Object.Services.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Swagger Example-Dapper-Migration API",
+        Description = "Base Swagger Description"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    s.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddScoped<SqlConnection>(s => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IDbTransaction>(s =>
+{
+    SqlConnection connection = s.GetRequiredService<SqlConnection>();
+    connection.Open();
+    return connection.BeginTransaction();
+});
+
+// Dependency Injections 
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+builder.Services
+    .AddScoped<IUnit_of_Work, Unit_of_Work>()
+    .AddScoped<ICompany_Repository, Company_Repository>()
+    .AddScoped<IEmployees_Repository, Employees_Repository>()
+    .AddScoped<IDepartment_Repository, Department_Repository>()
+    .AddScoped<IEmployeeServices, EmployeeServices>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(s =>
+    {
+        s.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger API Version");
+        s.RoutePrefix = string.Empty;
+    });
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Hospital_Management_System_DAL/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper_Data_Access_Layer.Base_Entity;
+
+namespace Dapper_Data_Access_Layer.Entities
+{
+    public class Department : Entity
+    {
+        public string Department_name { get; set; } = string.Empty;
+       
+        public Guid Clinic_id { get; set; }
+    }
+}
